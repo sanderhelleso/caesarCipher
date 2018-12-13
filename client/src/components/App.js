47ec339737d862f1c3c5db5c2d14baba06ccc504
Sanderhelleso <sanderhelleso@hotmail.com>
2018-12-13 00:37:30 -0800

    init commit

---
+++ client/src/components/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+    render() {
+        return (
+            <main>
+                <div className="columns ">
+                    <div id="hero" className="column">
+                        <h1 className="long-shadow">Casesar<br></br><span>Cipher</span></h1>
+                        <div id="hero-bg"></div>
+                    </div>
+                    <div className="column">
+                    </div>
+                </div>
+            </main>
+        )
+    }
+}
---
+++ client/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+import '../node_modules/bulma/css/bulma.min.css';
+
+ReactDOM.render(
+    <App />, 
+    document.querySelector('#root')
+);
